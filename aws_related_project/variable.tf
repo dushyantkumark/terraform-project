002618f82b304bfe4b7eda2d66213ad3e2d59eb9
dushyantkumar <kumardushyant545@gmail.com>
2023-06-09 11:11:12 +0530

    adding aws ec2, s3_bucket terraform project

---
+++ aws_related_project/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  default = "ami-007855ac798b5175e"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  default = "batch-demo-instance"
+}
+
+variable "bucket_name" {
+  default = "my-batch-test-bucket"
+}
